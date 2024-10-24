9d65c493c02cd17a5be217e5e08a75e1abf0a1f5
estervitoria21 <163008451+estervitoria21@users.noreply.github.com>
2024-10-24 18:08:17 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+// Seleciona o botão
+const button = document.getElementById('changeColorButton');
+
+// Array com cores
+const colors = ['#FF5733', '#33FF57', '#3357FF', '#F3FF33', '#FF33A8'];
+
+// Função para mudar a cor de fundo
+button.addEventListener('click', function() {
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.body.style.backgroundColor = randomColor;
+});
 
